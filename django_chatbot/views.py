c7b05ac235eaedca9f725917e9abb82f63138258
Mshah2003 <123729708+Mshah2003@users.noreply.github.com>
2023-10-26 16:12:50 +0530

    Add files via upload

---
+++ django_chatbot/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+# Create your views here.
+
+openai_api_key = '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'
+# openai_api_key = 'cmpl-7Aoqa4s48g3WBMdpPKadUdqtncPZ8'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = message,
+        max_tokens = 150,
+        n = 1,
+        stop = None,
+        temperature = 0.7,
+        )
+    print(response)
+    # answer = response.choice[0].text.strip()
+    # return answer
+    
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message':message,'response':response})
+    return render(request,'chatbot.html')
